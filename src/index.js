76cdbb1696ec35e8fba7e27e89dbbeb1693e965f
zhangenming <zhangenming@baidu.com>
2021-03-29 20:21:10 +0800

    FUCK ICODE
    Change-Id: I69249961a7874f51a734406abe10a686de02896d

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+import React from 'react';
 import ReactDOM from 'react-dom';
 import {lightTheme} from './theme';
 import Main from './main/View';
--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import {createMuiTheme} from '@material-ui/core/styles';
 import teal from '@material-ui/core/colors/teal';
 import purple from '@material-ui/core/colors/purple';
